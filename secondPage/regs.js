e8668eeecf47c407dd2d641f1d78d5f18dbb9087
mohammad2001ah <mah.2001.ah@gmail.com>
2025-01-30 14:51:21 +0300

    Update regs.js

--- secondPage/regs.js
+++ secondPage/regs.js
@@ -6,17 +6,30 @@ var passwordPage=document.getElementById('passwordPage')
 var userName=document.getElementById('userName');
 var country=document.getElementById('country');
 var registerForm=document.getElementById('registerForm');
+var userList= JSON.parse(localStorage.getItem('user'))||[]
 registerForm.addEventListener('submit',function(event){
     event.preventDefault();
-    localStorage.setItem('firstName', firstName.value);
-    localStorage.setItem('lastName', lastName.value);
-    localStorage.setItem('date', date.value);
-    localStorage.setItem('emailAddress', emailAddress.value);
-    localStorage.setItem('passwordPage', passwordPage.value);
-    localStorage.setItem('userName', userName.value);
-    localStorage.setItem('country', country.value);
+    // localStorage.setItem('firstName', firstName.value);
+    // localStorage.setItem('lastName', lastName.value);
+    // localStorage.setItem('date', date.value);
+    // localStorage.setItem('emailAddress', emailAddress.value);
+    // localStorage.setItem('passwordPage', passwordPage.value);
+    // localStorage.setItem('userName', userName.value);
+    // localStorage.setItem('country', country.value);
     localStorage.setItem('key','i am test')//set variable in localStorage
-    console.log(localStorage.getItem('key'))
+    console.log(localStorage.getItem('key'));
+    var user=
+        {
+            firstName:firstName.value,
+            lastName:lastName.value,
+            date:date.value,
+            emailAddress:emailAddress.value,
+            passwordPage:passwordPage.value,
+            userName:userName.value,
+            country:country.value
+        }
+    userList.push(user)
+    localStorage.setItem('user',JSON.stringify(userList))
     alert('successful register')
     window.location.href='/firstPage/login.html'
-})
+    })
